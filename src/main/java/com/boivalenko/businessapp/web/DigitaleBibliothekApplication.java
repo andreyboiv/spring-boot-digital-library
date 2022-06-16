46dd31742f9094af4683144f38fd4a924a616132
Andrey Boivalenko <andrey.boiv@gmail.com>
2022-06-16 21:51:21 +0200

    Initial commit

---
+++ src/main/java/com/boivalenko/businessapp/web/DigitaleBibliothekApplication.java
@@ -0,0 +1,13 @@
+package com.boivalenko.businessapp.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitaleBibliothekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitaleBibliothekApplication.class, args);
+    }
+
+}
